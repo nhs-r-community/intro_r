cafa4249744eda36d7343a3cd28312f3c5162587
Tom Jemmett <tom.jemmett@gmail.com>
2021-06-21 14:36:49 +0100

    downloads fontawesome

--- .github/workflows/rebuild_slides.R
+++ .github/workflows/rebuild_slides.R
@@ -1,5 +1,7 @@
 setwd(here::here())
 
+icons::download_fontawesome()
+
 git <- function (..., echo_cmd = TRUE, echo = TRUE, error_on_status = TRUE)  {
   callr::run("git", c(...), echo_cmd = echo_cmd, echo = echo, 
              error_on_status = error_on_status)
