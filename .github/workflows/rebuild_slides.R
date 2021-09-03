6ec4a498cbf8f5611ef60a5c0f2b93294f1a4f19
Chris Mainey <c.mainey@nhs.net>
2021-09-03 12:17:39 +0100

    Added RSQLite install to github actions

--- .github/workflows/rebuild_slides.R
+++ .github/workflows/rebuild_slides.R
@@ -3,6 +3,8 @@ setwd(here::here())
 
 icons::download_fontawesome()
 
+install.packages("RSQLite")
+
 git <- function (..., echo_cmd = TRUE, echo = TRUE, error_on_status = TRUE)  {
   callr::run("git", c(...), echo_cmd = echo_cmd, echo = echo, 
              error_on_status = error_on_status)
